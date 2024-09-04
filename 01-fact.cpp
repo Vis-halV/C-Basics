93b49432ae4630cf056ad1937374cb4d19e6131f
Vis-halV <vishal06076@gmail.com>
2024-09-04 21:23:45 +0530

    Factorial of n

---
+++ 01-fact.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main() {
+    int n,i;
+    int fact=1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n)
+
+    for(i=1;i<=n;i++){
+        fact = fact * i;
+    }
+
+    printf("The factorial is : %d",fact);
+
+    return 0;
+}
