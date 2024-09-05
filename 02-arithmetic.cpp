07b979a0641901381242efaa5f8e74113c3c7a1d
Vis-halV <vishal06076@gmail.com>
2024-09-05 13:28:18 +0530

    Arithmetic operation

---
+++ 02-arithmetic.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    int y = 5;
+
+    // Addition
+
+    int sum = x + y; 
+    cout<<"The sum of "<< x << " and " << y << " is : " << sum <<endl;
+
+    // Subtraction
+
+    int diff = x - y; 
+    cout<<"The difference of "<< x << " and " << y << " is : " << diff <<endl;
+    
+    // Multipllication
+
+    int mul = x * y; 
+    cout<<"The sum of "<< x << " and " << y << " is : " << mul <<endl;
+
+    // Division
+
+    int div = x / y; 
+    cout<<"The divisor of "<< x << " and " << y << " is : " << div <<endl;
+
+    // Remainder
+
+    int r = x % y; 
+    cout<<"The sum of "<< x << " and " << y << " is : " << r <<endl;
+
+    return 0;
+
+}
