0833c4c74f32f937964951e6179660f861bef47c
Vis-halV <vishal06076@gmail.com>
2024-09-05 15:59:32 +0530

    Simple if else

---
+++ Basics/LECTURE-03/03-ifelse.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+
+    cout<< "Enter the number: "<<endl;
+    cin>> n;
+
+    if(n%2==0){
+        cout<<"Number is even.";
+    } else{
+        cout<<"Number is odd";
+    }
+        
+    return 0;
+}
---
+++ Basics/LECTURE-03/03-ternary.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int n = 45;
+
+    cout << (n >= 0 ? "Positive " : "Negative") << endl;
+    return 0;
+
+}
