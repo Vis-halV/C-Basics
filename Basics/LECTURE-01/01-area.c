84ed809c4b204c3754b1935d57df6a377374f32f
Vis-halV <vishal06076@gmail.com>
2024-09-06 18:59:29 +0530

    Area of square

--- Basics/LECTURE-01/01-area.c
+++ Basics/LECTURE-01/01-area.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
 
+// Basics understa=ndin of flowchar and algorithms //
+
 int main() {
-    int x,area;
+
+    // Declaration of variables //
+    int x,area; 
+
+    // Getting values for the var x //
     printf("Enter the side of the square to find its area:");
     scanf("%d", &x);
+
+    // Calculating the area and displaying //
     area = x * x;
     printf("The area of the square is: %d",area);
+
     return 0;
 }
