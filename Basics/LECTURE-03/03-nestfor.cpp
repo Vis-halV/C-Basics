c660c953c52531aaf3f9daeb7485a2343ec4ec3a
Vis-halV <vishal06076@gmail.com>
2024-09-06 19:44:02 +0530

    Conditional statements and Loops

--- Basics/LECTURE-03/03-nestfor.cpp
+++ Basics/LECTURE-03/03-nestfor.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main() {
-    int n = 4;
+    int n = 4;    // Global variable 
 
     for(int i=1 ; i<=n; i++) {
-        int m =4;
+        int m =4; // Local variable
 
         for(int j=1; j<=n; j++) {
             cout << "*";
         }
         cout << endl;
     }
+
+    
 
     return 0;
 }
