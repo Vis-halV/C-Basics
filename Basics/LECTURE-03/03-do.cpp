ec73a196b84a5b31937d0695cdee2bcfffa0a2ac
Vis-halV <vishal06076@gmail.com>
2024-09-06 18:40:02 +0530

    Do while loop

---
+++ Basics/LECTURE-03/03-do.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int i = 2;
+
+    do {
+
+        cout << "Hello frnds" << endl;  //The looop will once then the condition is checked .
+
+    } while (i < 1);
+    
+    return 0;
+}
