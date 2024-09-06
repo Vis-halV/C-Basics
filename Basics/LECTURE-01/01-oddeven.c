3f9a65990be3ea70d5ef47867ced4692380429fb
Vis-halV <vishal06076@gmail.com>
2024-09-06 19:08:48 +0530

    Basic flow and algo

--- Basics/LECTURE-01/01-oddeven.c
+++ Basics/LECTURE-01/01-oddeven.c
@@ -5,7 +5,8 @@ int main() {
     printf("Enter the number:");
     scanf("%d",&n);
 
-    // Checking whether or or even using remainder modulo (%) operator 
+    // Checking whether or or even using remainder modulo (%) operator //
+    
     if((n%2)==0){
         printf("Number is even.");
     } else{
