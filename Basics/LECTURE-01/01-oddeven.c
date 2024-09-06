d1458858be141c8ceb514f89e62ad96e57c9e737
Vis-halV <vishal06076@gmail.com>
2024-09-06 19:07:58 +0530

    Odd or even

--- Basics/LECTURE-01/01-oddeven.c
+++ Basics/LECTURE-01/01-oddeven.c
@@ -5,6 +5,7 @@ int main() {
     printf("Enter the number:");
     scanf("%d",&n);
 
+    // Checking whether or or even using remainder modulo (%) operator 
     if((n%2)==0){
         printf("Number is even.");
     } else{
