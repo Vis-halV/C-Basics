cfc4ac3dc33dbbba63b0e53199dbda9c06e980e1
Vis-halV <vishal06076@gmail.com>
2024-09-06 18:51:14 +0530

    Base for patterns

---
+++ Basics/LECTURE-03/03-nestfor.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for(int i=1 ; i<=n; i++) {
+        int m =4;
+
+        for(int j=1; j<=n; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
