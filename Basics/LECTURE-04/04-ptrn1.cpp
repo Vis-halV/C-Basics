252072eb094b697481766d1f88e7f4c5aab02b2d
Vis-halV <vishal06076@gmail.com>
2024-09-07 09:35:23 +0530

    square *4

--- Basics/LECTURE-04/04-ptrn1.cpp
+++ Basics/LECTURE-04/04-ptrn1.cpp
@@ -7,9 +7,11 @@ int main() {
     for(int i = 1; i<=n; i++){    // outer loop for columns
 
         for(int j =1; j<=n; j++){ // inner loop for rows
-            cout << "*";
+            cout << "*" << " ";
         }
 
         cout << endl;
     }
+
+    return 0;
 }
