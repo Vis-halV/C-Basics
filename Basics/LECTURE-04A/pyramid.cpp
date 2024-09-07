a52fcd2670262beda155b8d8f96933a88265f0d6
Vis-halV <vishal06076@gmail.com>
2024-09-07 23:00:33 +0530

    Pyramid num

--- Basics/LECTURE-04A/pyramid.cpp
+++ Basics/LECTURE-04A/pyramid.cpp
@@ -4,11 +4,14 @@ using namespace std;
 int main() {
     int n = 4;
 
-    for( int i = 0; i<n; i++){             // For n numbers of rows
-        for( int j = n - i - 1; j>0; j--){     // Loop for spaces 
+    for( int i = 0; i<n; i++){                  // For n numbers of rows
+        for( int j = n - i - 1; j>0; j--) {     // Loop for spaces 
             cout << " ";
         }
-        for( int j = 1; j <= i + 1; j++){         // Loop for first half numbers
+        for( int j = 1; j <= i + 1; j++) {      // Loop for first half numbers
+            cout << j;
+        }
+        for(int j = 1; j <= i; j++) {           // Loop for seconf half numbers
             cout << j;
         }
         cout << endl;
