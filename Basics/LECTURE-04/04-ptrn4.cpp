c7d0cd2021cd4305b2bedcc373bb97f9622839bc
Vis-halV <vishal06076@gmail.com>
2024-09-07 09:43:11 +0530

    A - I

---
+++ Basics/LECTURE-04/04-ptrn4.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 3;
+    char ch = 'A'; 
+
+    for(int i = 0; i<n; i++){
+
+        
+        for(int j =0; j<n; j++){    
+            cout << ch <<" ";
+            ch = ch + 1; 
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
