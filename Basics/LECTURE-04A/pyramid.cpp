bfccec3719d1547b314db05416a2643e9cd07d4e
Vis-halV <vishal06076@gmail.com>
2024-09-07 22:02:47 +0530

    Pyramid one half

---
+++ Basics/LECTURE-04A/pyramid.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for( int i = 0; i<n; i++){             // For n numbers of rows
+        for( int j = n - i - 1; j>0; j--){     // Loop for spaces 
+            cout << " ";
+        }
+        for( int j = 1; j <= i + 1; j++){         // Loop for first half numbers
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
