729c3ca41b7ca8b1d51dd2f90aa3252842d5bcf1
Vis-halV <vishal06076@gmail.com>
2024-09-07 09:23:20 +0530

    ABCD * 4

---
+++ Basics/LECTURE-04/04-ptrn2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for(int i = 0; i<n; i++){   // Outer loop
+
+        char ch = 'A';  // Declaring here makes every line starting again from A
+        for(int j =0; j<n; j++){    // Inner loop -> line start
+            cout << ch;
+            ch = ch + 1; // A = 65; B = 66 >>>>>> Type casting ...
+        }
+
+        cout << endl;
+    }
+}
