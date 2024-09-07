78525437e20d6aabe72f49b8c6be2d1b7f8da4c5
Vis-halV <vishal06076@gmail.com>
2024-09-07 21:19:21 +0530

    Inverted triangle alpha

---
+++ Basics/LECTURE-04A/inverteda.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for( int i = 0; i < n; i++){
+        
+        for( int j = 0; j < i; j++){   // Loop for spaces
+            cout << " ";
+        }
+        /*using j again is because after that loop it gets terminated .*/
+        
+        for( int j = 0; j < n-i; j++){ //Loop for alphabets
+            cout << char('A' + i);
+        }
+
+        cout << endl;
+    }
+}
