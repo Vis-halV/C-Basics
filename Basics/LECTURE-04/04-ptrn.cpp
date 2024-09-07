ec8e819f555c68c2ef3b09d9b563aac5234831fc
Vis-halV <vishal06076@gmail.com>
2024-09-07 09:25:41 +0530

    1234 * 4

---
+++ Basics/LECTURE-04/04-ptrn.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+
+    for(int i = 1; i<=n; i++){    // outer loop for columns
+
+        int x = 1;
+        for(int j =1; j<=n; j++){ // inner loop for rows
+            cout << x;
+            x++;
+        }
+
+        cout << endl;
+    }
+}
