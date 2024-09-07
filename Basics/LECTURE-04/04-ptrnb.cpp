268edb2ad62004d6214fe65f59ec8435139a317c
Vis-halV <vishal06076@gmail.com>
2024-09-07 19:42:05 +0530

    Floyd's tri-2

---
+++ Basics/LECTURE-04/04-ptrnb.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 4;
+    char ch = 'A';
+
+    for( int i = 1; i<=n; i++){
+
+        for( int j = 1; j<=i; j++){
+            cout << ch  << " ";
+            ch += 1;
+        }
+        
+        cout << endl;
+        
+    }
+
+    return 0;
+}
