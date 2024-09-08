c8de367ff6af4ff8d7235a5f10f60f1e136643ee
Vis-halV <vishal06076@gmail.com>
2024-09-08 11:06:06 +0530

    Hollow Diamond

--- Basics/LECTURE-04A/hollowdia.cpp
+++ Basics/LECTURE-04A/hollowdia.cpp
@@ -6,24 +6,42 @@ int main() {
 
     // TOP
     for( int i = 0; i<n; i++) {
-        for( int j = 0; j<n-i-1; j++){   // Spaces
-            cout << "  ";
+        for( int j = 0; j<n-i-1; j++) {   // Spaces
+            cout << " ";
         }
 
         cout << "*";                    // Stars
 
         /*to get two stars in every line except the first*/
 
-        if(i != 0){   
-            for( int j = 0; j<2*i-1; j++){   // Spaces
-            cout << "  ";
+        if(i != 0) {   
+            for( int j = 0; j<2*i-1; j++) {   // Spaces
+            cout << " ";
             }        
 
              cout << "*";
         }
 
         cout << endl;
+    }
 
+    // Bottom
+    for( int i = 0; i<n-1; i++) {
+        for( int j = 0; j<i+1; j++) {   // Spaces
+            cout << " ";
+        }
+
+        cout << "*";
+
+        if(i != n-2) {   
+            for( int j = 0; j<2*(n-i)-5; j++) {   // Spaces
+            cout << " ";
+            }        
+
+             cout << "*";
+        }
+
+        cout << endl;
     }
 
     return 0;
