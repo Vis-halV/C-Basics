f58afa5c1225d204c577fd5383b9d755531c0d23
Vis-halV <vishal06076@gmail.com>
2024-09-08 11:36:32 +0530

    Butterfly pattern

--- Basics/LECTURE-04A/butterfly.cpp
+++ Basics/LECTURE-04A/butterfly.cpp
@@ -26,5 +26,29 @@ int main() {
         cout << endl;
     }
 
+    for(int i  = 0; i<n-1; i++) {
+        for(int j = i; j<n-1; j++) {   // Stars
+            cout << "*";
+        }
+
+        for(int j = 0; j<=i; j++) {  // Inverted spaces
+            cout << " ";
+        }
+
+        
+        for(int j = 0; j<=i; j++) {  // Spaces
+            cout << " ";
+        }
+
+        for(int j = 0; j<n-i-1; j++) { // Inverted triangle for stars
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    /*Once you finish the top half you can rreuse the triangular block of code again for the bottom
+      By replacing in different block of tri*/
+
     return 0;
 }
