f20deda4493be1a94a32a6121f1eb8de90594476
Vis-halV <vishal06076@gmail.com>
2024-09-11 23:58:21 +0530

    Reverse a number

---
+++ Basics/LECTURE-07/revnum.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int sumDigits(int num) {
+    int digit = 0;
+
+    while(num > 0) {
+        int lastDigit = num % 10;
+        num /= 10;
+        digit *= 10;
+        digit += lastDigit;
+        
+    
+    }
+
+    return digit;
+}
+int main() {
+    cout << "Sum =" << sumDigits(1234) << endl; 
+
+    return 0;
+}
