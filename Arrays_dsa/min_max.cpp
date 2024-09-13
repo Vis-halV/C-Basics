6786bf0511fffda9271c25419468ee3a94329fb3
Vis-halV <vishal06076@gmail.com>
2024-09-13 20:11:44 +0530

    Smallest and largest numbers in a array

--- Arrays_dsa/min_max.cpp
+++ Arrays_dsa/min_max.cpp
@@ -2,18 +2,23 @@
 using namespace std;
 
 int main() {
-    int num[] ={5 , 56, -34 -5 , 0, 10};
+    int num[] ={5 , 56, -34, -5 , 0, 10};
     int size = 6;
 
     int min = INT_FAST8_MAX;
+    int max = INT_FAST8_MIN;
 
     for(int i = 0; i < size; i++) {
         if(num[i] < min) {
             min = num[i];
         }
+        if(num[i] > max) {
+            max = num[i];
+        }
     }
      
     cout << "Smallest number : " << min << endl;
+    cout << "Largest number : " << max << endl;
 
     return 0;
 }
