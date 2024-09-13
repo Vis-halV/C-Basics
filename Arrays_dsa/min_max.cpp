24c4312c14e1735cad246fe0d70c0f755c09adf1
Vis-halV <vishal06076@gmail.com>
2024-09-13 21:03:19 +0530

    Min and max terms with index

--- Arrays_dsa/min_max.cpp
+++ Arrays_dsa/min_max.cpp
@@ -7,18 +7,22 @@ int main() {
 
     int min = INT_FAST8_MAX;
     int max = INT_FAST8_MIN;
+    int min_index, max_index;
 
     for(int i = 0; i < size; i++) {
         if(num[i] < min) {
             min = num[i];
+            min_index =  i + 1;
+
         }
         if(num[i] > max) {
             max = num[i];
+            max_index = i + 1; 
         }
     }
      
-    cout << "Smallest number : " << min << endl;
-    cout << "Largest number : " << max << endl;
+    cout << "Smallest number : " << min << " found at " << min_index << endl;
+    cout << "Largest number : " << max << " found at " << max_index << endl;
 
     return 0;
 }
