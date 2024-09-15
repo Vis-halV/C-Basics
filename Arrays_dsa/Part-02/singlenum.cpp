9e4e5a6f3674a2a5e0ca5e26d0d62e2e06be3f2c
Vis-halV <vishal06076@gmail.com>
2024-09-15 20:35:32 +0530

    Single number using X-OR bitwise op (^)

---
+++ Arrays_dsa/Part-02/singlenum.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for( int i : nums) {
+            ans ^= i; // Bitwise X-OR operator eliminates same value to 0
+
+        }
+        return ans;
+    } 
+};
+
+int main() {
+    Solution  s;
+    vector<int> nums = {1, 2, 4, 2, 1};
+    cout << "The given vector is 1 2 4 2 1 " << endl;
+    cout << "The unique/single number is " << s.singleNumber(nums) << endl;
+
+    return 0;
+}
