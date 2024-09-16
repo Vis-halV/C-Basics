cc783ac968d08b5432125e86bd57c7dd7a9951b7
Vis-halV <vishal06076@gmail.com>
2024-09-16 20:49:14 +0530

    Including proper header functions

--- Arrays_dsa/Part-02/LECTURE-10/max-subArr.cpp
+++ Arrays_dsa/Part-02/LECTURE-10/max-subArr.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
 int main() {
     int n = 5;
     int arr[5] = {1, 2, 3, 4, 5};
 
-    int maxSum = INT8_MIN;
+    int maxSum = INT_MIN;
     
     for(int st = 0; st < n; st++){
         int sum = 0;
