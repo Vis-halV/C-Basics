f1ff0ee2360fc037d1c10b574a89f0d87426efc1
Vis-halV <vishal06076@gmail.com>
2024-09-16 20:19:45 +0530

    Maximun subarray using Brute Force

---
+++ Arrays_dsa/Part-02/LECTURE-10/max-subArr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n = 5;
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int maxSum = INT8_MIN;
+    
+    for(int st = 0; st < n; st++){
+        int sum = 0;
+        for(int end = st; end < n; end++){
+            sum += arr[end];
+            maxSum = max(sum, maxSum);
+        }
+        cout << endl;
+    }
+
+    cout << "Max subarray sum = " << maxSum << endl;
+
+    return 0;
+}
