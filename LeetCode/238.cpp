a12dd8d6ee16f06741931f202aa7c91050ea168e
Vis-halV <vishal06076@gmail.com>
2024-09-24 22:33:54 +0530

    Product of Array Except Self

--- LeetCode/238.cpp
+++ LeetCode/238.cpp
@@ -4,28 +4,23 @@ using namespace std;
 
 vector<int> productExceptSelf(vector<int>& nums) {
     int n = nums.size();
-    vector<int> ans(n, 1); 
-    vector<int> pre(n, 1); 
-    vector<int> suf(n, 1);
+    vector<int> ans(n, 1); // Initializing with value 1
 
-    // Prefix
-    for(int i = 1; i < n; i++) {
-        pre[i] = pre[i-1] * nums[i-1];
-    } 
+    // Prefix 
+    for (int i = 1; i < n; i++) {
+        ans[i] = ans[i - 1] * nums[i - 1];
+    }
 
-    // Suffix
-    for(int i = n-2; i >= 0; i--) {
-        suf[i] = suf[i+1] * nums[i-1];
-    }      
+    int suf = 1;
 
-    // Answer
-    for(int i = 0; i < n; i++) {
-        ans[i] = pre[i] * suf[i];
-    }   
+    // Suffix 
+    for (int i = n - 2; i >= 0; i--) {
+        suf *= nums[i + 1];
+        ans[i] *= suf;
+    }
 
     return ans;
 }
-
 
 int main() {
     vector <int> nums = {1, 2, 3, 4};
