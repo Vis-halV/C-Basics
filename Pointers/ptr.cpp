a5acb5e2168baf61da4278b7ffccb1819fef8451
Vis-halV <vishal06076@gmail.com>
2024-09-25 23:57:56 +0530

    Dereferencing operator

--- Pointers/ptr.cpp
+++ Pointers/ptr.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
     int a = 10;
-    cout << &a << endl;
+    cout << *(&a) << endl;
 
     int *ptr = &a;
-    cout << ptr << endl;
+    cout << *(ptr) << endl;
 
     
     return 0;
