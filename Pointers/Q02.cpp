e0d51f2fa0a03a1392259f9922cc083ed65f9b46
Vis-halV <vishal06076@gmail.com>
2024-09-27 23:34:28 +0530

    Predict output

---
+++ Pointers/Q02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {10, 20, 30, 40};
+    int *ptr = arr;
+
+    cout << *(ptr + 1) << endl;
+    cout << *(ptr + 3) << endl;
+    ptr++;
+    cout << *ptr << endl;
+
+    return 0;
+}
