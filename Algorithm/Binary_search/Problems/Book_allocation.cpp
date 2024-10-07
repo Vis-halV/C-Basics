e3d7272470b7c50edb481569ffe12a49e68d5fe4
Vis-halV <vishal06076@gmail.com>
2024-10-07 22:30:50 +0530

    Book Allocation

--- Algorithm/Binary_search/Problems/Book_allocation.cpp
+++ Algorithm/Binary_search/Problems/Book_allocation.cpp
@@ -2,6 +2,25 @@
 #include <vector>
 using namespace std;
 
+bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages) {
+    int stud = 1, pages = 0;
+
+    for(int i = 0; i<n; i++) {
+        if(arr[i] > maxAllowedPages) {
+            return false;
+        }
+
+        if(pages + arr[i] <= maxAllowedPages) {
+            pages += arr[i];
+        } else {
+            stud++;
+            pages = arr[i];
+        }
+    }
+
+    return stud > m ? false : true;
+}
+
 int allocateBooks(vector<int> &arr, int n, int m) {
     if(m > n){
         return -1;
@@ -16,9 +35,9 @@ int allocateBooks(vector<int> &arr, int n, int m) {
     int st = 0, end = sum;  // Range of possible values
 
     while(st <= end) {
-        int mid = st + (st-end)/2;
+        int mid = st + (end - st) / 2;
 
-        if(isValid(arr, n, m)) {    // LEFT
+        if(isValid(arr, n, m, mid)) {    // LEFT
             ans = mid;
             end = mid - 1;
         } else {                    // RIGHT
@@ -31,7 +50,7 @@ int allocateBooks(vector<int> &arr, int n, int m) {
 
 int main() {
     vector<int> arr = {2, 1, 3, 4};
-    int n = 4, m =2;
+    int n = 4, m = 2;
 
     cout << allocateBooks(arr, n , m) << endl;
 
