47c58e6e9e0452c92d73e585aaf239e937eaba5e
Vis-halV <vishal06076@gmail.com>
2024-10-08 23:51:43 +0530

    Main function for BST

---
+++ Algorithm/Binary_search/Problems/Painters_partition.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int minTimeToPaint(vector<int> &arr, int n, int m){
+    int sum = 0, maxVal = INT8_MIN;
+    for(int i = 0; i < n; i++){
+        sum += arr[i];
+        maxVal = max(maxVal, arr[i]);
+    }
+    
+    int st = maxVal, end = sum, ans = -1;
+
+    while(st <= end) {
+        int mid = st + (end - st)/2;
+
+        if(isPossible(arr, n, m, mid)) {
+            ans = mid;end = mid - 1;
+        } else {
+            st = mid + 1;
+        }
+    }
+
+}
+
+int main() {
+    vector<int> arr = {40, 30, 10, 20};
+    int n = 4, m = 2;
+
+    cout << minTimeToPaint(arr, n, m) << endl;
+    return 0;
+}
