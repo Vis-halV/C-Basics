cadd8dca130e6832f7546de8077a1aaf52a2900e
Vis-halV <vishal06076@gmail.com>
2024-10-12 23:11:56 +0530

    Optimised for sorted array

--- Algorithm/Sorting/Bubble_sort.cpp
+++ Algorithm/Sorting/Bubble_sort.cpp
@@ -3,10 +3,17 @@ using namespace std;
 
 void bubbleSort(int arr[], int n) { //n^2
     for(int i = 0; i<n-1; i++) {
+        bool isSwap = false;    // To prevent sorted array
+
         for(int j = 0; j<n-i-1; j++) {
             if(arr[j] > arr[j+1]) {
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
+        }
+
+        if(!isSwap) {   // Array is sorted 
+            return;
         }
     }
 }
