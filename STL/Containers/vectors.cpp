5dc71d34eea1e5a62d67ed0c1b5cbe20152676bd
Vis-halV <vishal06076@gmail.com>
2024-10-16 19:30:29 +0530

    Push back and pop back

--- STL/Containers/vectors.cpp
+++ STL/Containers/vectors.cpp
@@ -10,20 +10,34 @@ void printVec(vector <int> vec) {
     cout << endl;
 }
 
+void printSizeCap(vector<int> vec) {
+    cout << "The size of vector is : " << vec.size() << endl;
+    cout << "The capacity of vector is : " << vec.capacity() << endl;
+}
+
 int main() {
     vector<int> vec;
-    cout << "The size of vector is : " << vec.size() << endl;
-
+    printSizeCap(vec);
     vec = {1, 2};
+
     cout << "The updated vector : ";
     printVec(vec);
-    cout << "The size of vector is : " << vec.size() << endl;
+    printSizeCap(vec);
+    
+    vec.push_back(3);
+    vec.push_back(4);
+    cout << "Updated vector using push back :" ;
+    printVec(vec);
+    printSizeCap(vec);
 
-    vector<int> A(3,10);
-    vector<int> B(A);
-
-    printVec(A);    
-    printVec(B);
+    vec.push_back(5);
+    cout << "Updated vector using push back :" ;
+    printVec(vec);
+    printSizeCap(vec);
+    
+    vec.pop_back();
+    cout << "After pop back : ";
+    printVec(vec);
 
     return 0;
 }
