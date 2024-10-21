2ede36bf9af5902b61e6ee5661ae49b36378a43b
Vis-halV <vishal06076@gmail.com>
2024-10-21 21:52:10 +0530

    String

---
+++ Strings/Part-01/char.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char str[] = {'a', 'b', 'c', '\0'}; // Strings literals
+    cout << strlen(str) << endl;    // constant Pointers
+
+    return 0;
+}
