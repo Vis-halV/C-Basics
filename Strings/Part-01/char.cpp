b8698716f28dcc2d77cc72f9d6446c9491f4d493
Vis-halV <vishal06076@gmail.com>
2024-10-22 20:02:35 +0530

    Char Array

--- Strings/Part-01/char.cpp
+++ Strings/Part-01/char.cpp
@@ -3,8 +3,22 @@
 using namespace std;
 
 int main() {
-    char str[] = {'a', 'b', 'c', '\0'}; // Strings literals
-    cout << strlen(str) << endl;    // constant Pointers
+    char str[] = "Hello World"; // Strings literals
+    int len = 0;
 
+    // cout << "Enter you char arr : "; 
+    // cin.getline(str, 100, '$');
+
+    for(int i = 0; i<str[i] != '\0'; i++) {
+        len++;
+    }
+
+    cout << "Lenght : " << len << endl;
+
+    for(char ch : str) {
+        cout << ch <<  " ";
+    }
+
+    cout << endl;
     return 0;
 }
