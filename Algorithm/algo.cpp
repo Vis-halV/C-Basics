26ec50eea929e6a525aa9096a3492a60e5120562
Vis-halV <vishal06076@gmail.com>
2024-10-26 23:58:32 +0530

    #include <algorithm>

---
+++ Algorithm/algo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    vector<int> vec = {1, 2, 3, 4, 5};
+    reverse(vec.begin(), vec.end());
+
+    for(auto val : vec) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    cout << "Max_element : " << max_element(vec.begin(), vec.end());
+    cout << "Min_element : " << min_element(vec.begin(), vec.end());
+
+    cout << "Max of 4 and 5 : " << max(4,5) << endl;
+    cout << "Min of 4 and 5 : " << min(4,5) << endl;
+
+    int a = 6, b = 9;
+    cout << "Before swap a = " << a << " and b = " << b << endl;
+    swap(a,b);
+    cout << "After swap a = " << a << " and b = " << b << endl;
+
+    return 0;
+}
