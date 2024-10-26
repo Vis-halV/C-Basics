ff11503888092a8b256e2ffc907467ae931dc9c5
Vis-halV <vishal06076@gmail.com>
2024-10-27 00:01:17 +0530

    #include <algorithm>

--- Algorithm/algo.cpp
+++ Algorithm/algo.cpp
@@ -12,8 +12,8 @@ int main() {
     }
     cout << endl;
 
-    cout << "Max_element : " << max_element(vec.begin(), vec.end());
-    cout << "Min_element : " << min_element(vec.begin(), vec.end());
+    cout << "Max_element : " << *(max_element(vec.begin(), vec.end())) << endl;
+    cout << "Min_element : " << *(min_element(vec.begin(), vec.end())) << endl;
 
     cout << "Max of 4 and 5 : " << max(4,5) << endl;
     cout << "Min of 4 and 5 : " << min(4,5) << endl;
